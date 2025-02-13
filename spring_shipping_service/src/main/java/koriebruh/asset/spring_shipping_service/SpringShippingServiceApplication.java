7a2741f57cc2c3361d0134e214d646956abc1670
koriebruh <koriebruh@gmail.com>
2025-02-13 12:18:16 +0700

    add service shipping

---
+++ spring_shipping_service/src/main/java/koriebruh/asset/spring_shipping_service/SpringShippingServiceApplication.java
@@ -0,0 +1,13 @@
+package koriebruh.asset.spring_shipping_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringShippingServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringShippingServiceApplication.class, args);
+    }
+
+}
---
+++ spring_shipping_service/src/test/java/koriebruh/asset/spring_shipping_service/SpringShippingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package koriebruh.asset.spring_shipping_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringShippingServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
